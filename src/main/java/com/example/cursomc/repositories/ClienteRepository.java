39c5444af2003803c352146cd96f80e34b395f56
Estevão Manhães <estevaoe298@gmail.com>
2020-11-11 10:47:27 -0300

    Cliente, TipoCliente, Telefone e Endereco

--- src/main/java/com/example/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/example/cursomc/repositories/ClienteRepository.java
@@ -4,11 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.cursomc.domain.Categoria;
-import com.example.cursomc.domain.Cidade;
-import com.example.cursomc.domain.Endereco;
-import com.example.cursomc.domain.Estado;
+import com.example.cursomc.domain.Cliente;
 
 @Repository
-public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
 }
--- src/main/java/com/example/cursomc/repositories/EnderecoRepository.java
+++ src/main/java/com/example/cursomc/repositories/EnderecoRepository.java
@@ -4,11 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.cursomc.domain.Categoria;
-import com.example.cursomc.domain.Cidade;
+import com.example.cursomc.domain.Cliente;
 import com.example.cursomc.domain.Endereco;
-import com.example.cursomc.domain.Estado;
 
 @Repository
-public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
 
 }
