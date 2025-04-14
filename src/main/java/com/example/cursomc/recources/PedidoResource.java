fbee9c8051a3da044d6b2eaf30b6d6f9da0c04cc
Estevao Manhaes <estevaoe298@gmail.com>
2025-04-14 15:08:03 -0300

    Inserinndo pedido.

--- src/main/java/com/example/cursomc/recources/PedidoResource.java
+++ src/main/java/com/example/cursomc/recources/PedidoResource.java
@@ -1,14 +1,17 @@
 package com.example.cursomc.recources;
 
+import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.dto.CategoriaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.cursomc.domain.Pedido;
 import com.example.cursomc.services.PedidoService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value="/pedidos")
@@ -22,6 +25,15 @@ public class PedidoResource {
 		
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		System.out.println(">>> Chegou no insert()");
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
